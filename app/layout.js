b0f0ec4c0c3799096b62aadfdb6012ad267dbd43
Abhinav1730 <abhinav.saxena1730@gmail.com>
2025-04-21 19:39:27 +0530

    Setting up of the project

---
+++ app/layout.js
@@ -0,0 +1,29 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "AI Interview Assistance Agent",
+  description: "A powerful application for students available free to take up mock interviews based on their qualification with the help of AI voice agent.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
